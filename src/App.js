8a5ff53dcb6a6bb8f0fbd0db9712b78dce646488
Vyshak Ramesh <vyshak.ramesh@mindcurv.com>
2024-06-12 10:50:10 +0530

    book table functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      Home Page
-    </div>
+    <>
+      <Nav />
+      <Main />
+      <Menu />
+      <Footer />
+    </>
   );
 }
 
